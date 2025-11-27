0958d0a2226db4c0a82e8ed92f5f17c01fa80e0a
Its-Delimas <spencerdelimas@gmail.com>
2025-11-27 17:41:06 +0300

    //////////////////////

--- 0000/src/pages/Home/home.jsx
+++ 0000/src/pages/Home/home.jsx
@@ -1,9 +1,20 @@
 import Navbar from "./components/navbar";
+import "./home.css";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <div className="Hero-section">
+        <h2>Hello There 🖐🏾 </h2>
+        <p>
+          my name is Spencer Delimas <br />
+          I,m a{" "}
+          <h1>
+            | Full Stack <span>Developer</span> |
+          </h1>
+        </p>
+      </div>
     </div>
   );
 }
